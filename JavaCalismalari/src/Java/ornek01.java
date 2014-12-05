beb1fe1bb3a8facf477e849cd633b60863f89026
USER <USER@10.50.13.146>
2014-12-05 14:54:36 +0200

    first example

---
+++ JavaCalismalari/src/Java/ornek01.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class ornek01 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		System.out.println("BTBS");
+		System.out.println("Java");
+		System.out.printf("Sayý: %d",5);
+		System.out.printf("Ad :%s","fatih");
+	}
+}
